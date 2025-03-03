75eb8fa9ab652ab6bf6571aadcaa4538b71d79cb
Oz-Nazhad <l.weinberger@tutamail.com>
2025-03-03 10:39:42 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=3")
+vim.cmd("set softtabstop=3")
+vim.cmd("set shiftwidth=3")
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
